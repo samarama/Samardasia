ab7f3b659ebc5ef02e72c3f4b54c2388b60d6e5a
Ewa Samardakiewicz <ewa.es7@gmail.com>
2018-06-09 21:31:23 +0200

    Commit message

---
+++ lapucapu.py
@@ -0,0 +1,13 @@
+def get_help():
+    print('To jest prosty program kalkulatora.  Wprowadź dwie liczby i zatwierdź enterem.')
+
+
+def dodawanie(a, b):
+    wynik = a + b
+    return wynik
+
+
+get_help()
+zm1 = int(input())
+zm2 = int(input())
+print(dodawanie(zm1, zm2))
